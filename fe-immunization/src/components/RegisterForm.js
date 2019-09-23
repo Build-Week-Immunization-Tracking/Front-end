bc6d21d232f89f228c6357aa768f854f9a236ab1
Adan <51211285+Adan011@users.noreply.github.com>
2019-09-23 18:08:19 -0500

    up to date with frontend

--- fe-immunization/src/components/RegisterForm.js
+++ fe-immunization/src/components/RegisterForm.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { Form, Grid, Segment } from 'semantic-ui-react';
+import { Form, Checkbox, Button, Label } from 'semantic-ui-react';
 import axios from 'axios';
 
 export default function RegisterForm(props) {
     const [userCredentials, setCredentials] = useState({
-        firstname: "",
-        lastname: "",
         username: "",
         password: ""
     });
@@ -27,22 +25,21 @@ export default function RegisterForm(props) {
 
     return (
         <div>
-            <Grid textAlign="center" verticalAlign="middle" style={{ height: "100vh" }}>
-                <Grid.Column>
-
-                    <Form onSubmit={submitHandler}>
-                        <Segment>
-                            <h1>Register</h1>
-                            <Form.Input fluid type="text" placeholder="First Name" name="firstname" onChange={changeHandler} value={userCredentials.firstname} required />
-                            <Form.Input fluid type="text" placeholder="Last Name" name="lastname" onChange={changeHandler} value={userCredentials.lastname} required />
-                            <Form.Input fluid type="text" placeholder="Username" name="username" onChange={changeHandler} value={userCredentials.username} required />
-                            <Form.Input fluid type="password" placeholder="Password" name="password" onChange={changeHandler} value={userCredentials.password} required />
-
-                            <Form.Button fluid content="Register" size="large" />
-                        </Segment>
-                    </Form>
-                </Grid.Column>
-            </Grid>
+            <Form onSubmit={submitHandler}>
+                <h1>Register</h1>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input placeholder='First Name' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input placeholder='Last Name' />
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' />
+                </Form.Field>
+                <Button type='submit'>Submit</Button>
+            </Form>
         </div>
     )
 }
